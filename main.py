8283d025448744fea847ae6cb2638151a2a95a87
AndreyAD <arakelyants.andrey@yandex.ru>
2020-10-17 18:07:54 +0300

    Print received data.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import asyncio
 
 
+HOST = 'minechat.dvmn.org'
+PORT = 5000
+
+
+async def tcp_client():
+    reader, writer = await asyncio.open_connection(HOST, PORT)
+    try:
+        while True:
+            received_data = await reader.read(1000)
+            print(received_data.decode().rstrip('\n'))
+    except asyncio.CancelledError:
+        raise
+    finally:
+        writer.close()
+
+
 def main():
-    pass
+    asyncio.run(tcp_client())
 
 
 if __name__ == '__main__':
