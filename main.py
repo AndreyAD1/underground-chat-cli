4b4417a0ee11a004d0e17f2682538e38f9f72822
AndreyAD <arakelyants.andrey@yandex.ru>
2020-10-17 18:38:11 +0300

    Write a chat history to the file.

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import asyncio
+from datetime import datetime
+
+import aiofiles
 
 
 HOST = 'minechat.dvmn.org'
 PORT = 5000
 
 
-async def tcp_client():
+async def main():
     reader, writer = await asyncio.open_connection(HOST, PORT)
     try:
         while True:
             received_data = await reader.read(1000)
-            print(received_data.decode().rstrip('\n'))
+            async with aiofiles.open('chat_history.txt', 'a') as history_file:
+                message = received_data.decode()
+                formatted_datetime = datetime.now().strftime('%d.%m.%y %H:%M')
+                log_note = f'{formatted_datetime} {message}'
+                await history_file.write(log_note)
     except asyncio.CancelledError:
         raise
     finally:
         writer.close()
 
 
-def main():
-    asyncio.run(tcp_client())
-
-
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
