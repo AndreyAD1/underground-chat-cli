7687275771cb41a474c341fe9d2723cb76a437f9
AndreyAD <arakelyants.andrey@yandex.ru>
2020-10-20 11:52:51 +0300

    Setup logging.

--- send_message.py
+++ send_message.py
@@ -1,23 +1,35 @@
 import asyncio
+import logging
 
 
 ACCOUNT_HASH = 'e54a90d6-11d8-11eb-8c47-0242ac110002\n'
+logger = logging.getLogger('sender')
+logger.setLevel(logging.DEBUG)
+for handler in logger.handlers:
+    logger.removeHandler(handler)
+
+console = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+console.setFormatter(formatter)
+logger.addHandler(console)
 
 
 async def send_message():
     reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5050)
     server_response = await reader.readline()
-    print('Server response', server_response.decode())
-    print(f'Send message: {ACCOUNT_HASH}')
+    logger.debug(repr(server_response.decode()))
+    logger.debug(repr(ACCOUNT_HASH))
     writer.write(ACCOUNT_HASH.encode())
     server_response = await reader.readline()
-    print(f'Server response: {server_response.decode()}')
+    logger.debug(repr(server_response.decode()))
     server_response = await reader.readline()
-    print(f'Server response: {server_response.decode()}')
+    logger.debug(repr(server_response.decode()))
+    message = 'SPAAAAAM\n'
     writer.write('SPAAAAAM\n'.encode())
     writer.write(b'\n')
+    logger.debug(repr(message))
     server_response = await reader.readline()
-    print(f'Server response: {server_response.decode()}')
+    logger.debug(repr(server_response.decode()))
     writer.close()
 
 
